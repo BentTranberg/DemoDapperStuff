d4c79fec19f5596dba0612dcd2c403a5b4cf1a49
Bent Tranberg <bent.tranberg@gmail.com>
2021-04-04 13:53:19 +0200

    Deleting tables. Inserting entities.

--- DemoDapperStuff/Da.fs
+++ DemoDapperStuff/Da.fs
@@ -6,6 +6,20 @@ open Dapper
 open Dapper.Contrib
 open Dapper.Contrib.Extensions
 
+let ofSeq (s: 'T seq) = new ResizeArray<'T>(s)
+
+let truncateTable (conn: SqlConnection, tableName) =
+    conn.Execute ("TRUNCATE TABLE " + tableName)
+
+let deleteAllTableRecords (conn: SqlConnection, tableName) =
+    conn.Execute ("DELETE FROM " + tableName)
+
+let insertEntity (conn: SqlConnection, entity: 'T) : int =
+    conn.Insert entity |> int
+
+let insertEntities (conn: SqlConnection, list: 'T list) =
+    list |> ofSeq |> conn.Insert |> ignore
+
 let queryMultipleAsSeq<'T> (conn: SqlConnection, sql: string, args: obj) : 'T seq =
     conn.Query<'T> (sql, args)
 
--- DemoDapperStuff/Program.fs
+++ DemoDapperStuff/Program.fs
@@ -16,6 +16,18 @@ let [<Literal>] tableUser = "[User]" // "User" is a keyword, so the brackets are
 [<Table (tableUser); CLIMutable>]
 type EntUser = { Id: int; UserName: string; Role: string }
 
+let truncateUsersTable () =
+    use conn = new SqlConnection(connectionString)
+    Da.truncateTable (conn, tableUser)
+
+let addUser (user: EntUser) =
+    use conn = new SqlConnection(connectionString)
+    Da.insertEntity (conn, user) |> ignore
+
+let addUsers (users: EntUser list) =
+    use conn = new SqlConnection(connectionString)
+    Da.insertEntities (conn, users) |> ignore
+
 let getUsers () =
     use conn = new SqlConnection(connectionString)
     (conn, "SELECT * FROM " + tableUser, null)
@@ -28,11 +40,27 @@ let getUsersWithRole (role: string) =
 
 [<EntryPoint>]
 let main _ =
+    truncateUsersTable () |> ignore // Start with a clean slate.
+
+    printfn "Add our first user."
+    addUser { Id = 0; UserName = "Frodo"; Role = "Baggins" }
+
+    printfn "Add more users."
+    let add u r = { Id = 0; UserName = u; Role = r }
+    addUsers [
+        add "Aragorn" "king"
+        add "Isildur" "king"
+        add "Sauron" "evil"
+        add "Arwen" "elf"
+        add "Galadriel" "elf"
+        add "Bilbo" "BAGGINS"
+    ]
+
     printfn "List of all users"
     getUsers ()
     |> List.iter (fun user -> printfn "  Id=%d User=%s" user.Id user.UserName)
 
-    printfn "List of admin users"
+    printfn "List of role Baggins."
     getUsersWithRole "baggins"
     |> List.iter (fun user -> printfn "  Id=%d User=%s Role=%s" user.Id user.UserName user.Role)
 
